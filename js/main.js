657db4b9bc345511a5e252aa05ed0f2e8ef9487b
WesleySwart <wswart2008@gmail.com>
2014-06-03 17:54:52 -0500

    Moved image template to main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ app.controller('MyCtrl', function($scope){
 	$scope.gridOptions = {
 		data: 'myData',
 		columnDefs: [{field: 'original', displayName: 'Original', width: 90},
-		{field: 'preview', displayName: 'Preview', width: 90, cellTemplate: 'imageTemplate.html'}]
+		{field: 'preview', displayName: 'Preview', width: 90, cellTemplate: '<div ng-class="{\'tile.tile-2-preview .tile-inner\': row.getProperty(\'preview\') == \'2.jpg\'}"><div class="ngCellText">{{row.getProperty(\'preview\')}}</div></div>'}]
 	};
 
 });
+//'imageTemplate.html'}]
+//'<div class="ngCellText" ng-class="col.colIndex()"><a ng-click="loadById(row)">{{row.getProperty(col.field)}}</a></div>' 
